9051a2845e2506f45072143a0f37fe39edb3bac8
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2025-02-25 23:46:58 +0000

    fix: new convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,14 +8,14 @@
  * @module
  */
 
-import type * as http from "../http.js";
-import type * as stats from "../stats.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as http from "../http.js";
+import type * as stats from "../stats.js";
+
 /**
  * A utility for referencing Convex functions in your app's API.
  *
@@ -28,143 +28,11 @@ declare const fullApi: ApiFromModules<{
   http: typeof http;
   stats: typeof stats;
 }>;
-declare const fullApiWithMounts: typeof fullApi;
-
 export declare const api: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-export declare const components: {
-  ossStats: {
-    github: {
-      getGithubOwners: FunctionReference<
-        "query",
-        "internal",
-        { owners: Array<string> },
-        Array<null | {
-          contributorCount: number;
-          dependentCount: number;
-          dependentCountPrevious?: { count: number; updatedAt: number };
-          dependentCountUpdatedAt?: number;
-          name: string;
-          nameNormalized: string;
-          starCount: number;
-          updatedAt: number;
-        }>
-      >;
-      updateGithubOwner: FunctionReference<
-        "mutation",
-        "internal",
-        { name: string },
-        any
-      >;
-      updateGithubOwnerStats: FunctionReference<
-        "action",
-        "internal",
-        { githubAccessToken: string; owner: string; page?: number },
-        any
-      >;
-      updateGithubRepoStars: FunctionReference<
-        "mutation",
-        "internal",
-        { name: string; owner: string; starCount: number },
-        any
-      >;
-      updateGithubRepos: FunctionReference<
-        "mutation",
-        "internal",
-        {
-          repos: Array<{
-            contributorCount: number;
-            dependentCount: number;
-            name: string;
-            owner: string;
-            starCount: number;
-          }>;
-        },
-        any
-      >;
-    };
-    lib: {
-      clearAndSync: FunctionReference<
-        "action",
-        "internal",
-        {
-          githubAccessToken: string;
-          githubOwners: Array<string>;
-          minStars: number;
-          npmOrgs: Array<string>;
-        },
-        any
-      >;
-      clearPage: FunctionReference<
-        "mutation",
-        "internal",
-        { tableName: "githubRepos" | "npmPackages" },
-        { isDone: boolean }
-      >;
-      clearTable: FunctionReference<
-        "action",
-        "internal",
-        { tableName: "githubRepos" | "npmPackages" },
-        null
-      >;
-      sync: FunctionReference<
-        "action",
-        "internal",
-        {
-          githubAccessToken: string;
-          githubOwners: Array<string>;
-          minStars: number;
-          npmOrgs: Array<string>;
-        },
-        null
-      >;
-    };
-    npm: {
-      getNpmOrgs: FunctionReference<
-        "query",
-        "internal",
-        { names: Array<string> },
-        Array<null | {
-          dayOfWeekAverages: Array<number>;
-          downloadCount: number;
-          downloadCountUpdatedAt: number;
-          name: string;
-          updatedAt: number;
-        }>
-      >;
-      updateNpmOrg: FunctionReference<
-        "mutation",
-        "internal",
-        { name: string },
-        any
-      >;
-      updateNpmOrgStats: FunctionReference<
-        "action",
-        "internal",
-        { org: string; page?: number },
-        any
-      >;
-      updateNpmPackagesForOrg: FunctionReference<
-        "mutation",
-        "internal",
-        {
-          org: string;
-          packages: Array<{
-            dayOfWeekAverages: Array<number>;
-            downloadCount: number;
-            isNotFound?: boolean;
-            name: string;
-          }>;
-        },
-        any
-      >;
-    };
-  };
-};
